71c0a30569c2a523b404d809c95f99702b461dbe
Dcorum <dpcorum@yahoo.com>
2018-02-26 14:06:12 -0700

    small fixes

--- src/util/SaveFile.java
+++ src/util/SaveFile.java
@@ -2,11 +2,13 @@ package util;
 
 import java.util.ArrayList;
 
+import item.Items;
+import models.Monster;
 import models.Player;
 
 public class SaveFile {
 
 	ArrayList<Player> players = new ArrayList<>();
 	ArrayList<Monster> monsters = new ArrayList<>();
-	ArrayList<Item> items = new ArrayList<>();
+	ArrayList<Items> items = new ArrayList<>();
 }
