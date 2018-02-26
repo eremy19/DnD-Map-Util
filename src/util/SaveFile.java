9f80793c9ddb5f99d0f51c3d89c60c9083a5f9b4
Jett Tripp <jtripp@student.neumont.edu>
2018-02-26 12:47:27 -0700

    Added SaveFile.java

---
+++ src/util/SaveFile.java
@@ -0,0 +1,11 @@
+package util;
+
+import java.util.ArrayList;
+import models.Player;
+
+public class SaveFile {
+
+	ArrayList<Player> players = new ArrayList<>();
+	ArrayList<Monster> monsters = new ArrayList<>();
+	ArrayList<Item> items = new ArrayList<>();
+}
