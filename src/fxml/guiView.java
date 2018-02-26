0e8cbd301a554ff9dcae577079f3e74e657f3e3a
eremy19 <emilyremy@gmail.com>
2018-02-26 14:05:48 -0700

    fixing push errors

---
+++ src/fxml/guiView.java
@@ -0,0 +1,55 @@
+package fxml;
+
+
+import javafx.application.Application;
+import javafx.beans.property.ObjectProperty;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class guiView extends Application{
+
+	public static void runWindow() {
+		launch();
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Parent root = FXMLLoader.load(getClass().getResource("OOPproject.fxml"));
+		
+		Scene scene = new Scene(root,1200,800);
+		stage.setScene(scene);
+		stage.setAlwaysOnTop(true);
+		stage.setResizable(false);
+		stage.show();
+	}
+	
+
+	// need button mouse click listener to switch to Map
+	public final ObjectProperty<EventHandler<? super MouseEvent>> onMouseClickedProperty(){
+		
+		Parent root = FXMLLoader.load(getClass().getResource("OOPproject.fxml"));
+		Scene scene = new Scene(root,1200,800);
+		  Stage stage = (Stage)((Node)onMouseClickedProperty().getSource()).getScene().getWindow();
+		 
+	        stage.setScene(scene);
+			stage.setAlwaysOnTop(true);
+			stage.setResizable(false);
+			stage.show();
+		return null;
+	}
+	
+	// need button mouse click listener to switch to Entity Editor
+	public final ObjectProperty<EventHandler<? super MouseEvent>> onMouseClickedProperty1(){
+
+		return null;
+	}
+	
+	
+}
