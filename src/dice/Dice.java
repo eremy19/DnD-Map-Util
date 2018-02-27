45207032e1d17ffd01726e980de2362e4f2bd08a
eremy19 <emilyremy@gmail.com>
2018-02-27 13:02:37 -0700

    Can now roll dice with rollDice method

--- src/dice/Dice.java
+++ src/dice/Dice.java
@@ -1,6 +1,7 @@
 package dice;
 
 import java.util.HashMap;
+import java.util.Random;
 
 public class Dice {
 	HashMap<String, Integer> diceHM = new HashMap<>();
@@ -13,6 +14,12 @@ public class Dice {
 		diceHM.put("d12", 12);
 		diceHM.put("d20", 20);
 		diceHM.put("d100", 100);
+	}
+	
+	public int rollDice(String diceName) {
+		Random random = new Random();
+		int roll = random.nextInt(diceHM.get(diceName))+1;
+		return roll;
 	}
 	
 }
