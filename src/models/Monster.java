80465ce0c9992ba9d22855398888ee843d579875
Dcorum <dpcorum@yahoo.com>
2018-02-27 14:42:20 -0700

    changed controller class

--- src/models/Monster.java
+++ src/models/Monster.java
@@ -3,12 +3,16 @@ package models;
 public class Monster extends Mob {
 
 	public Monster(String name, int maxHP, int strength, int dexterity, int constitution, int inteligence, int wisdom,
-			int charisma, int armor, int spellAttackBonus, int spellCastingAbility, int speed) {
+			int charisma, int armor, int spellAttackBonus, int spellCastingAbility, int speed, int initive,
+			String hitDie) {
 		super(name, maxHP, strength, dexterity, constitution, inteligence, wisdom, charisma, armor, spellAttackBonus,
-				spellCastingAbility, speed);
-		
+				spellCastingAbility, speed, initive, hitDie);
+		// TODO Auto-generated constructor stub
+	}
+
+	
 	}
 
 	
 
-}
+
--- src/models/Player.java
+++ src/models/Player.java
@@ -3,12 +3,15 @@ package models;
 public class Player extends Mob {
 
 	public Player(String name, int maxHP, int strength, int dexterity, int constitution, int inteligence, int wisdom,
-			int charisma, int armor, int spellAttackBonus, int spellCastingAbility, int speed) {
+			int charisma, int armor, int spellAttackBonus, int spellCastingAbility, int speed, int initive,
+			String hitDie) {
 		super(name, maxHP, strength, dexterity, constitution, inteligence, wisdom, charisma, armor, spellAttackBonus,
-				spellCastingAbility, speed);
-		
+				spellCastingAbility, speed, initive, hitDie);
+		// TODO Auto-generated constructor stub
 	}
 
 	
 
-}
+	}
+
+
