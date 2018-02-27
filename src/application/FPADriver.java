19e73e648e9e25351ede34ba92b95c5f141e519e
Dcorum <dpcorum@yahoo.com>
2018-02-27 15:42:11 -0700

    managed to change entitybutton text in controller class

--- src/application/FPADriver.java
+++ src/application/FPADriver.java
@@ -12,24 +12,30 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class FPADriver extends Application {
+	String path = "../fxml/MapView.fxml";
+
 
 	@Override
 	public void start(Stage Stage) throws IOException {
-		FXMLLoader loader = new FXMLLoader((getClass().getResource("../fxml/OOPproject.fxml")));
+		FXMLLoader loader = new FXMLLoader((getClass().getResource(path)));
 		Parent root = loader.load();
+		
 		UIController controller = loader.getController();
+		
+		controller.setMain(this);
+		
 		Scene scene = new Scene(root, 1200, 800);
 		Stage.setScene(scene);
 		Stage.setAlwaysOnTop(true);
 		Stage.setResizable(false);
 		Stage.show();
 
-		controller.entityButton.addEventFilter(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				controller.entityButton.setText("YAY!!");
-			}
-		});
+//		controller.entityButton.addEventFilter(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
+//			@Override
+//			public void handle(ActionEvent event) {
+//				controller.entityButton.setText("YAY!!");
+//			}
+//		});
 	}
 
 	public static void main(String[] args) {
--- src/controllers/UIController.java
+++ src/controllers/UIController.java
@@ -1,14 +1,23 @@
 package controllers;
 
+import application.FPADriver;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class UIController {
 
-	public Button entityButton;
+	@FXML
+	private Button entityButton;
 
-	public void buttonClicked() {
+	private FPADriver FPAD;
 
+	public void setMain(FPADriver FPAD) {
+		this.FPAD = FPAD;
+	}
+
+	public void handleButton() {
+		entityButton.setText("Changed");
 	}
 }
