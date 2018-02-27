36af1ab25321cd329db856b9a160802c292c6d74
eremy19 <emilyremy@gmail.com>
2018-02-27 12:00:54 -0700

    Revert "bad save"
    This reverts commit eef41a4fc285efe244fe4c4889aeb65cbfd51b66.

---
+++ src/fxml/guiView.java
@@ -0,0 +1,101 @@
+package fxml;
+
+
+import java.beans.EventHandler;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class guiView extends Application implements Initializable{
+	private Stage currentStage;
+	private String filePath = "OOPproject.fxml";
+
+	public static void runWindow() {
+		launch();
+	}
+	@Override
+	public void start(Stage stage) throws IOException{
+		Parent root = FXMLLoader.load(getClass().getResource("OOPproject.fxml"));
+		Scene scene = new Scene(root,1200,800);
+		stage.setScene(scene);
+		stage.setAlwaysOnTop(true);
+		stage.setResizable(false);
+//		this is what is registering any and all mouse events. This was for me to test and debug and use for later.
+//		change the ".ANY" to another filter to see that filter
+		 scene.addEventFilter(MouseEvent.ANY, (event) ->{ // "(event) ->" is a Lambda Expression; to treat functionality as method argument, or code as data.
+			 System.out.println("Target name: "+event.getTarget());
+			 System.out.println("Event registered: "+ event.getEventType().getName());
+		 });
+		stage.show();
+	}
+	
+	/*
+	 * Notes on buttons
+	 * added   fx:controller="fxml.guiView"   to fxml grid pane declaration
+	 * In fxml File Change button id="" to fx:id=""
+	 * @FXML Button EntitySceneSwap <---takes button id and allows you to have events on it
+	 * initialize method works for setting the method needed for every button on MapView
+	 */
+	@FXML
+	Button EntitySceneSwap;
+	@FXML
+	ChoiceBox MapSelect;
+//	@FXML
+//	Stage Scene;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		//add action to button  
+		EntitySceneSwap.setOnAction(this::changeToMap);
+		//add options to combo box
+		MapSelect.getItems().addAll("Option A", "Option B", "Option C");
+		//sets the first value they see in the dropdown
+		MapSelect.setValue("Option A");
+		//listens for when user changes option in box
+		// doesnt do much now, but here we can send the "new_value" to a method to display the correct map
+		MapSelect.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+          public void changed(ObservableValue ov, Number value, Number new_value) {
+            MapSelect.setValue(new_value);
+          }
+        });
+
+	}
+	
+	private void changeToMap(ActionEvent event) {
+		if (EntitySceneSwap.getText().equals("Entities")) {
+			EntitySceneSwap.setText("Clicked Entities");
+		} else {
+			EntitySceneSwap.setText("Entities");
+		}
+	}
+	
+	/*
+	 * Attempts made to swap scenes:
+	 * having a method+button where it uses stage.setScene(): error in try/catches
+	 * @FXML MapSceneSwap button from Entity Scene doesnt work without the FXML Loader being for that fxml.
+	 * re-calling start with "current stage" and using currentFilepath for the FXML Loader: error somehow 
+	 * Having a second java file for the other FXML and scene: Error, cannot call launch() twice
+	 */
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
