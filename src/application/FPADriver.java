10406c1d33a21266b3b67ecbd23f4e45dc8467e3
Jett Tripp <jtripp@student.neumont.edu>
2018-02-28 18:01:57 -0700

    Set up initial load cant test yet, leaving initial load method commented out

--- src/application/FPADriver.java
+++ src/application/FPADriver.java
@@ -24,86 +24,89 @@ import util.SaveFile;
 
 public class FPADriver extends Application {
 
-String pathMap = "MapView.fxml";
-String pathEntity = "EntityView.fxml";
-Scene sceneMap, sceneEntity;
+	String pathMap = "MapView.fxml";
+	String pathEntity = "EntityView.fxml";
+	Scene sceneMap, sceneEntity;
 
-ArrayList<Map> maps;
-ArrayList<Player> players;
-ArrayList<Monster> monsters;
-ArrayList<Items> items;
-
-
+	ArrayList<Map> maps;
+	ArrayList<Player> players;
+	ArrayList<Monster> monsters;
+	ArrayList<Items> items;
 
 	@Override
 	public void start(Stage Stage) throws IOException {
-		
-		initialLoad();
-		
-//		FXMLLoader loader = new FXMLLoader((getClass().getResource(mapPath)));
-//		FXMLLoader loader = new FXMLLoader((getClass().getResource(entityPath)));
+
+//		initialLoad();
+
+		// FXMLLoader loader = new FXMLLoader((getClass().getResource(mapPath)));
+		// FXMLLoader loader = new FXMLLoader((getClass().getResource(entityPath)));
 		FXMLLoader loader = new FXMLLoader((getClass().getResource(pathMap)));
 		FXMLLoader loader2 = new FXMLLoader((getClass().getResource(pathEntity)));
-		
-		
 
 		Parent root = loader.load();
 		Parent root2 = loader2.load();
-		
+
 		UIController controller = loader.getController();
 		EntityController controller2 = loader2.getController();
-		
+
 		controller.setMain(this);
-		
+
 		sceneMap = new Scene(root, 1200, 800);
-		sceneEntity = new Scene(root2, 1200,800);
+		sceneEntity = new Scene(root2, 1200, 800);
 		Stage.setScene(sceneMap);
 		Stage.setAlwaysOnTop(false);
 		Stage.setResizable(false);
 		Stage.show();
-		
+
 		controller.entitySceneSwap.setOnAction(e -> Stage.setScene(sceneEntity));
 		controller2.entities.setOnAction(e -> Stage.setScene(sceneMap));
-//		controller2.entities.addEventFilter(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
+		// controller2.entities.addEventFilter(ActionEvent.ACTION, new
+		// EventHandler<ActionEvent>() {
 
-//			@Override
-//			public void handle(ActionEvent event) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//		});
-//		controller.entityButton.addEventFilter(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
-//			@Override
-//			public void handle(ActionEvent event) {
-//				controller.entityButton.setText("YAY!!");
-//			}
-//		});
+		// @Override
+		// public void handle(ActionEvent event) {
+		// // TODO Auto-generated method stub
+		//
+		// }
+		// });
+		// controller.entityButton.addEventFilter(ActionEvent.ACTION, new
+		// EventHandler<ActionEvent>() {
+		// @Override
+		// public void handle(ActionEvent event) {
+		// controller.entityButton.setText("YAY!!");
+		// }
+		// });
 	}
 
 	private void initialLoad() {
 
 		String filePath = "saveFileObject";
-		
+		SaveFile sf = new SaveFile(items, players, monsters, maps);
 		try {
-			SaveFile sf = loadFile(filePath);
+			sf = loadFile(filePath);
 		} catch (ClassNotFoundException e) {
 			System.out.println("There was an error loading the startup file.");
 		} catch (IOException e) {
 			System.out.println("There was an error loading the startup file.");
 		}
-		
+
+		maps = sf.mapList;
+		players = sf.playerList;
+		monsters = sf.monsterList;
+		items = sf.itemList;
+
 	}
 
 	public static SaveFile loadFile(String filePath) throws IOException, ClassNotFoundException {
 		FileInputStream fis = new FileInputStream(filePath);
 		BufferedInputStream bis = new BufferedInputStream(fis);
 		ObjectInputStream ois = new ObjectInputStream(bis);
-		SaveFile sf = (SaveFile)ois.readObject();
+		SaveFile sf = (SaveFile) ois.readObject();
 		ois.close();
 		return sf;
 	}
-	
-	public static void saveFile (SaveFile sf, String filePath) throws IOException {
+
+	public static void saveFile(SaveFile sf, String filePath) throws IOException {
 		FileOutputStream fos = new FileOutputStream(filePath);
 		BufferedOutputStream bos = new BufferedOutputStream(fos);
 		ObjectOutputStream oos = new ObjectOutputStream(bos);
--- src/util/SaveFile.java
+++ src/util/SaveFile.java
@@ -3,19 +3,19 @@ package util;
 import java.util.ArrayList;
 
 import item.Items;
-import models.Chest;
 import models.Monster;
+import models.Player;
 
 public class SaveFile {
 	
 	public final ArrayList<Items> itemList;
-	public final ArrayList<Chest> chestList;
+	public final ArrayList<Player> playerList;
 	public final ArrayList<Monster> monsterList;
 	public final ArrayList<Map> mapList;
 	
-	public SaveFile(ArrayList<Items> itemList, ArrayList<Chest> chestList, ArrayList<Monster> monsterList, ArrayList<Map> mapList) {
+	public SaveFile(ArrayList<Items> itemList, ArrayList<Player> playerList, ArrayList<Monster> monsterList, ArrayList<Map> mapList) {
 		this.itemList = itemList;
-		this.chestList = chestList;
+		this.playerList = playerList;
 		this.monsterList = monsterList;
 		this.mapList = mapList;
 	}
