b77f96a13cf03851ecf2aedb341a6c8bb388d9f9
eremy19 <emilyremy@gmail.com>
2018-03-02 13:43:51 -0700

    monster button  added

--- src/controllers/EntityController.java
+++ src/controllers/EntityController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import models.Mob;
+import models.Monster;
 import models.Player;
 
 public class EntityController {
@@ -14,12 +15,17 @@ public class EntityController {
 
 	
 	public Button CharacterCreate;
-	
-	public void generateBlank(){
+	public Button MonsterCreate;
+
+	public void generateBlankPlayer(){
 		Player mobBlank = new Player("BlankSlate", 10,10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, "d4");
 		System.out.println(mobBlank.toString());
 		FPADriver.addPlayer(mobBlank);
 	}
-	
+	public void generateBlankMonster(){
+		Monster mobBlank = new Monster("BlankMonster", 10,10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, "d4");
+		System.out.println(mobBlank.toString());
+		FPADriver.addMonster(mobBlank);
+	}
 	
 }
