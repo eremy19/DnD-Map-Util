0f4139b520a58c789f4760244cb9a2f348f89a98
Jett Tripp <jtripp@student.neumont.edu>
2018-03-02 13:08:21 -0700

    Revert "added gridlines again"
    This reverts commit b978d24ddb9680c65c57e4bd5f2ec47f1bfbfba0.

--- src/application/FPADriver.java
+++ src/application/FPADriver.java
@@ -80,10 +80,7 @@ public class FPADriver extends Application {
 				GridPane.setConstraints(p, i, j);
 				controller.mapGrid.getChildren().add(p);
 				p.setOnMouseClicked(e -> {
-					p.setStyle(controller.color + "; -fx-border-color: black; -fx-border-width: 0.5;");
-				});
-				p.setOnDragDetected(e -> {
-					p.setStyle(controller.color + "; -fx-border-color: black; -fx-border-width: 0.5;");
+					p.setStyle(controller.color);
 				});
 			}
 		}
