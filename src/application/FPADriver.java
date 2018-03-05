9083e123e64ad8b3e19ca27b8e556e939e0c8b67
Jett Tripp <jtripp@student.neumont.edu>
2018-03-05 13:43:20 -0700

    Added future functionality for selecting maps, also changed some minor naming issues

--- src/application/FPADriver.java
+++ src/application/FPADriver.java
@@ -38,10 +38,10 @@ public class FPADriver extends Application {
 	String pathEntity = "EntityView.fxml";
 	Scene sceneMap, sceneEntity;
 
-	static ArrayList<Map> maps = new ArrayList<>();
-	static ArrayList<Player> players = new ArrayList<>();
-	static ArrayList<Monster> monsters = new ArrayList<>();
-	static ArrayList<Items> items = new ArrayList<>();
+	public static ArrayList<Map> maps = new ArrayList<>();
+	public static ArrayList<Player> players = new ArrayList<>();
+	public static ArrayList<Monster> monsters = new ArrayList<>();
+	public static ArrayList<Items> items = new ArrayList<>();
 
 	ArrayList<Pane> mapContents = new ArrayList<>();
 
